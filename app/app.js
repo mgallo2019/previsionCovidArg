b31cb963ef6fce0fc256796f55a210e529483263
Miguel Gallo <49215615+mgallo2019@users.noreply.github.com>
2020-07-10 19:10:13 -0300

    CompleteTable

--- app/app.js
+++ app/app.js
@@ -8,6 +8,8 @@ $(document).ready(function()
     var lotesProcess = 0;//variable para sincronizacion de lotes y mostrado de elementos
     let screen = window.matchMedia("(max-width: 560px)"); //variable que es usada para determinar el UMBRAL de cambio de RESOLUCION
     let targetScreen = 0;
+    var cacheData = [];
+    var requestsError = false;
 
     scroll();
 
@@ -69,10 +71,6 @@ $(document).ready(function()
                     console.log("Enviando!");
                 },//que haga algo antes que se envie
 
-                complete: function () {                
-                    console.log("Complete!");
-                },
-
                 success: function (response) {
                     console.log("TERMINO UN REQUEST:" + lote);
                     result = response.dates;
@@ -81,61 +79,25 @@ $(document).ready(function()
                 error: function (response) {
                     console.log("OCURRIO UN ERROR");
                     console.log("No se ha podido obtener la información");
+                    requestsError = true;
                     console.log(response);
                 },
             })
-        ).then(function() {
-            loadData(result);
+        ).then(function() {      
+            
+            loadJsonData(result);
+            
             lotesProcess = lotesProcess+1;
 
             //llegue al ultimo? muestro tabla
             if (lotesProcess == cantLotes){
-
-                //escondo el msge de carga
-                $("#loading").hide();
-        
-
-                 //tiene que llamarse luego de llenada la tabla
-                $('#tablaProb').DataTable(
-                    {
-                        responsive: true,
-                        "language": {
-                            "sProcessing":     "Procesando...",
-                            "sLengthMenu":     "Mostrar _MENU_ registros",
-                            "sZeroRecords":    "No se encontraron resultados",
-                            "sEmptyTable":     "Ningún dato disponible en esta tabla",
-                            "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                            "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-                            "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-                            "sInfoPostFix":    "",
-                            "sSearch":         "Buscar:",
-                            "sUrl":            "",
-                            "sInfoThousands":  ",",
-                            "sLoadingRecords": "Cargando...",
-                            "oPaginate": {
-                                "sFirst":    "Primero",
-                                "sLast":     "Último",
-                                "sNext":     "Siguiente",
-                                "sPrevious": "Anterior"
-                            },
-                            "oAria": {
-                                "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-                                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-                            },
-                            "buttons": {
-                                "copy": "Copiar",
-                                "colvis": "Visibilidad"
-                            }
-
-                        },
-
-                        "pagingType": "full_numbers",
-                        "pageLength": 50
-                    }
-                ).page('last').draw('page');;//uso de plugin apuntando a la ultima pagina!
-                
-                
-                $("#tblDinamica").show();
+                //todos los request fueron OK?
+                if(!requestsError){
+                    requestComplete();
+                }
+                else{
+                    requestError();
+                }    
             }
         });
     }
@@ -173,60 +135,78 @@ $(document).ready(function()
 
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    function loadData(objeto){
-
-        //var ultimaFecha;
-        //var previsionMañana = 0;
+    //se guardara los datos del request en un cache para luego tener los datos correlativos y ordenados
+    function loadJsonData(objeto){
 
         $.each(objeto, function(fecha, elemento) {
 
-            //ultimaFecha = fecha;
-            //la prevision sera cero para el primer dato
-            var prevision = 0;
-
-            //obtengo los datos del dia anterior
-            prevision = calculatePrevisionLastDay(objeto, fecha);
-
             $.each(elemento.countries, function(index, elemento1) {
-
-                // console.log(fecha, elemento1.today_new_confirmed, elemento1.today_confirmed,
-                //     elemento1.today_new_deaths,elemento1.today_deaths,
-                //     elemento1.today_new_recovered,elemento1.today_recovered);
-
-                //previsionMañana = Math.round(elemento1.today_new_confirmed * (elemento1.today_confirmed/elemento1.yesterday_confirmed));
-
-                var localLocale = moment(fecha);
-                moment.locale('es');
-                localLocale.locale(false);
-            
-                if (elemento1.today_new_confirmed > prevision){
-                    var post = `
-                        <tr>
-                            <td><span>${fecha}</span>${localLocale.format('LL')}</td>
-                            <td>${elemento1.today_new_confirmed}</td>
-                            <td>${elemento1.today_confirmed}</td>
-                            <td>${(elemento1.today_confirmed/elemento1.yesterday_confirmed).toFixed(3)}</td>
-                            <td style="background:rgb(182, 38, 38);color:white;">${prevision}</td>
-                        </tr>
-                    `;
-                }
-                else{
-                    var post = `
-                        <tr>
-                            <td><span>${fecha}</span>${localLocale.format('LL')}</td>
-                            <td>${elemento1.today_new_confirmed}</td>
-                            <td>${elemento1.today_confirmed}</td>
-                            <td>${(elemento1.today_confirmed/elemento1.yesterday_confirmed).toFixed(3)}</td>
-                            <td style="background:green;color:white;">${prevision}</td>
-                        </tr>
-                    `;
-                }
-                $('#tablaCuerpo').append(post);
+                //lleno array 
+                cacheData.push(
+                    {
+                        dia: fecha,
+                        infectadosNuevos: elemento1.today_new_confirmed,
+                        infectadosAlDia: elemento1.today_confirmed,
+                        infectadosAlDiaAyer: elemento1.yesterday_confirmed,
+                        muertesNuevos:  elemento1.today_new_deaths,
+                        muertesAlDia: elemento1.today_deaths,
+                        recuperadosNuevos: elemento1.today_new_recovered,
+                        recuperadosAlDia: elemento1.today_recovered
+                    }
+                );                
             });  
+        });
+    }
+
+    function custom_sort(a, b) {
+        return new Date(a.dia).getTime() - new Date(b.dia).getTime();
+    }
+    
+    function prepareData(objeto){
+
+        var ultimoDia;
+        var previsionMañana = 0;
+
+        objeto.forEach((item, index) => {
+
+            //la prevision sera cero para el primer dato
+            var prevision = calculatePrevisionLastDay(objeto, item.dia); //obtengo los datos del dia anterior
+      
+            //busco el ultimo dato
+            ultimoDia = item.dia;
+            previsionMañana = Math.round(item.infectadosNuevos * (item.infectadosAlDia/item.infectadosAlDiaAyer));
+
+            var localLocale = moment(item.dia);
+            moment.locale('es');
+            localLocale.locale(false);
+        
+            if (item.infectadosNuevos > prevision){
+                var post = `
+                    <tr>
+                        <td><span>${item.dia}</span>${localLocale.format('LL')}</td>
+                        <td>${item.infectadosNuevos}</td>
+                        <td>${item.infectadosAlDia}</td>
+                        <td>${(item.infectadosAlDia/item.infectadosAlDiaAyer).toFixed(3)}</td>
+                        <td style="background:rgb(182, 38, 38);color:white;">${prevision}</td>
+                    </tr>
+                `;
+            }
+            else{
+                var post = `
+                    <tr>
+                        <td><span>${item.dia}</span>${localLocale.format('LL')}</td>
+                        <td>${item.infectadosNuevos}</td>
+                        <td>${item.infectadosAlDia}</td>
+                        <td>${(item.infectadosAlDia/item.infectadosAlDiaAyer).toFixed(3)}</td>
+                        <td style="background:green;color:white;">${prevision}</td>
+                    </tr>
+                `;
+            }
+            $('#tablaCuerpo').append(post);
         });
 
         //llegue al ultimo
-        //addFinalRow(moment(ultimaFecha , 'YYYY-MM-DD').add(1, 'days').format('YYYY-MM-DD'),previsionMañana);
+        addFinalRow(moment(ultimoDia , 'YYYY-MM-DD').add(1, 'days').format('YYYY-MM-DD'),previsionMañana);
     }
 
 
@@ -235,16 +215,12 @@ $(document).ready(function()
 
         var datosAyer = 0;
 
-        $.each(objeto, function(fecha, elemento) {
-
-            var hoy = moment(fecha).format('YYYY-MM-DD');
+        objeto.forEach((item, index) => {
+            var hoy = moment(item.dia).format('YYYY-MM-DD');
             var ayer = moment(fechaHoy).subtract(1, 'd').format('YYYY-MM-DD');
 
-            if (hoy == ayer){
-                $.each(elemento.countries, function(index, elemento1) {
-                    datosAyer = Math.round(elemento1.today_new_confirmed * (elemento1.today_confirmed/elemento1.yesterday_confirmed));
-
-                });
+            if (hoy == ayer){             
+                datosAyer = Math.round(item.infectadosNuevos * (item.infectadosAlDia/item.infectadosAlDiaAyer));
             }
         });
 
@@ -264,12 +240,86 @@ $(document).ready(function()
                 <td>0</td>
                 <td>0</td>
                 <td>0</td>
-                <td style="background:green;color:white;">${prevision}</td>
+                <td style="background:green;color:white;font-weight: bold;">${prevision}</td>
             </tr>
         `;
         
         $('#tablaCuerpo').append(post);
+    }
 
+
+    function requestComplete(){
+
+        //ordeno el lote de datos
+        cacheData.sort(custom_sort);
+
+        //carga datos tabla
+        prepareData(cacheData);
+        //prepara los graficos
+
+
+
+        //escondo el msge de carga
+        $("#loading").hide();
+
+
+         //tiene que llamarse luego de llenada la tabla
+        $('#tablaProb').DataTable(
+            {
+                responsive: true,
+                "language": {
+                    "sProcessing":     "Procesando...",
+                    "sLengthMenu":     "Mostrar _MENU_ registros",
+                    "sZeroRecords":    "No se encontraron resultados",
+                    "sEmptyTable":     "Ningún dato disponible en esta tabla",
+                    "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+                    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+                    "sInfoPostFix":    "",
+                    "sSearch":         "Buscar:",
+                    "sUrl":            "",
+                    "sInfoThousands":  ",",
+                    "sLoadingRecords": "Cargando...",
+                    "oPaginate": {
+                        "sFirst":    "Primero",
+                        "sLast":     "Último",
+                        "sNext":     "Siguiente",
+                        "sPrevious": "Anterior"
+                    },
+                    "oAria": {
+                        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                    },
+                    "buttons": {
+                        "copy": "Copiar",
+                        "colvis": "Visibilidad"
+                    }
+
+                },
+
+                "pagingType": "full_numbers",
+                "pageLength": 50
+            }
+        ).page('last').draw('page');;//uso de plugin apuntando a la ultima pagina!
+        
+        
+        $("#tblDinamica").show();
+        
+    }
+
+    function requestError(){
+
+        //escondo el msge de carga
+        $("#loading").hide();
+
+        var post = `
+            <div id="errorMsge">
+            <p><img src="img/error.png" width="5%" height="5%"/></p>
+            <p style="color:red;">OCURRIO UN ERROR AL CONSULTAR DATOS, INTENTE NUEVAMENTE MAS TARDE</p>
+            </div>
+        `;
+
+        $("#lastP").append(post);
     }
 
 
